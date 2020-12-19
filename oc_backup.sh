10a90e6dd0ebe4a97177c577fcea0f59b9d0c189
Julian Pömp <j.poemp@campus.lmu.de>
2020-12-19 18:12:23 +0100

    added CREATE_LOGFILE option and removed SHOW_ERRORS_ONLY option.

--- oc_backup.sh
+++ oc_backup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 # Title: OC Backup Script
 # Description: Creates a backup of the whole owncloud installation incl. SQL database.
 # Author: Julian Poemp
@@ -17,121 +17,181 @@ OC_DB_NAME=""
 OC_DB_USER=""
 OC_DB_PASSWORD=""
 BACKUP_DESTINATION=""
-SHOW_ERRORS_ONLY=false
+CREATE_LOGFILE=true
 
 cmd_zip_exists=false
 cmd_sqldump_exists=false
 missing_constants=""
 is_config_valid=false
 show_help=false
+time_stamp=$(date "+%Y-%m-%d_%H-%M-%S")
+
+errors_found=false
 
 POSITIONAL=()
-while [[ $# -gt 0 ]]
-do
-key="$1"
+while [[ $# -gt 0 ]]; do
+  key="$1"
 
-case $key in
-    -c|--conf)
+  case $key in
+  -c | --conf)
     CONF_PATH="$2"
     shift # past argument
     shift # past value
     ;;
-    -h|--help)
+  -h | --help)
     show_help=true
     shift # past argument
     shift # past value
     ;;
-    *)    # unknown option
+  *) # unknown option
     POSITIONAL+=("$1") # save it in an array for later
-    shift # past argument
+    shift              # past argument
     ;;
-esac
+  esac
 done
 set -- "${POSITIONAL[@]}"
 
 source "${CONF_PATH}"
 
 # FUNCTIONS
-function log {
-  if [ "${SHOW_ERRORS_ONLY}" != true ] ; then
-    echo "${1}"
+log() {
+  if [ "${1}" != "" ]; then
+    if [ "${CREATE_LOGFILE}" = true ]; then
+      echo "* $(get_current_timestamp) | ${1}" | tee -a "${BACKUP_DESTINATION}/${time_stamp}_log.txt"
+    else
+      echo "* $(get_current_timestamp) | ${1}"
+    fi
   fi
 }
 
-function check_available_commands {
-  if type "zip" &> /dev/null; then
+check_cosuccess() {
+  stdout=$("${1}" 2>stderr.txt)
+  stderr=$(cat stderr.txt)
+
+  log "${stdout}"
+  log "Error: ${stderr}"
+
+  echo "" >stderr.txt
+
+  echo 0
+}
+
+check_available_commands() {
+  if type "zip" &>/dev/null; then
     cmd_zip_exists=true
   else
     cmd_zip_exists=false
   fi
 
-  if type "mysqldump" &> /dev/null; then
+  if type "mysqldump" &>/dev/null; then
     cmd_sqldump_exists=true
   else
-    cmd_sqldump_exists=true
+    cmd_sqldump_exists=false
   fi
 }
 
-function check_config {
-  if [ "${OC_INSTALLATION_PATH}" = "" ] ; then
+check_config() {
+  if [ "${OC_INSTALLATION_PATH}" = "" ]; then
     missing_constants="OC_INSTALLATION_PATH, "
   fi
 
-  if [ "${OC_DATA_PATH}" = "" ] ; then
-      missing_constants="${missing_constants}OC_DATA_PATH, "
+  if [ "${OC_DATA_PATH}" = "" ]; then
+    missing_constants="${missing_constants}OC_DATA_PATH, "
   fi
 
-  if [ "${OC_DB_NAME}" = "" ] ; then
-      missing_constants="${missing_constants}OC_DB_NAME, "
+  if [ "${OC_DB_NAME}" = "" ]; then
+    missing_constants="${missing_constants}OC_DB_NAME, "
   fi
 
-  if [ "${OC_DB_USER}" = "" ] ; then
-      missing_constants="${missing_constants}OC_DB_USER, "
+  if [ "${OC_DB_USER}" = "" ]; then
+    missing_constants="${missing_constants}OC_DB_USER, "
   fi
 
-  if [ "${OC_DB_PASSWORD}" = "" ] ; then
-      missing_constants="${missing_constants}OC_DB_PASSWORD, "
+  if [ "${OC_DB_PASSWORD}" = "" ]; then
+    missing_constants="${missing_constants}OC_DB_PASSWORD, "
   fi
 
-  if [ "${BACKUP_DESTINATION}" = "" ] ; then
-      missing_constants="${missing_constants}BACKUP_DESTINATION"
+  if [ "${BACKUP_DESTINATION}" = "" ]; then
+    missing_constants="${missing_constants}BACKUP_DESTINATION"
   fi
 
-  if [ "${missing_constants}" = "" ] ; then
+  if [ "${missing_constants}" = "" ]; then
     is_config_valid=true
   else
     is_config_valid=false
   fi
 }
 
-function enable_maintenance_mode {
+enable_maintenance_mode() {
   log "-> Enable maintenance mode..."
-  configFile="${configFile//\'maintenance\' => false/\'maintenance\' => true}"
+  configFile="${configFile//\'maintenance\' => false/'maintenance' => true}"
   echo "${configFile}" > "${configPath}"
 }
 
-function disable_maintenance_mode {
+disable_maintenance_mode() {
   log "-> Disbale maintenance mode..."
-  configFile="${configFile//\'maintenance\' => true/\'maintenance\' => false}"
+  configFile="${configFile//\'maintenance\' => true/'maintenance' => false}"
   echo "${configFile}" > "${configPath}"
 }
 
-function create_zip_backup {
+create_zip_backup() {
   log "-> Create zip archive of owncloud files..."
-  if [ "${SHOW_ERRORS_ONLY}" != true ] ; then
-    zip -r -s 1000m "${BACKUP_DESTINATION}/${timeNow}_owncloud.zip" "${OC_INSTALLATION_PATH}" "${OC_DATA_PATH}"
-  else
-    zip -r -s 1000m "${BACKUP_DESTINATION}/${timeNow}_owncloud.zip" "${OC_INSTALLATION_PATH}" "${OC_DATA_PATH}" &> /dev/null
+
+  stdout=$(zip -r -s 1000m "${BACKUP_DESTINATION}/${time_stamp}_owncloud.zip" "${OC_INSTALLATION_PATH}" "${OC_DATA_PATH}" 2>stderr.txt)
+  stderr=$(cat stderr.txt)
+
+  echo "${stdout}" >> "${BACKUP_DESTINATION}/${time_stamp}_log.txt"
+  log "${stderr}"
+
+  if [ "${#stderr}" -gt 0 ]; then
+    log "Error: Zip creation failed."
+    errors_found=$((errors_found + 1))
   fi
+
+  echo "" >stderr.txt
 }
 
-function doBackup {
-  configPath="${OC_INSTALLATION_PATH}/config/config.php"
+create_sql_backup() {
+  log "-> Create database backup..."
 
-  timeNow=$(date "+%Y-%m-%d_%H-%M-%S")
+  stdout=$(mysqldump -u "${OC_DB_USER}" -p"${OC_DB_PASSWORD}" "${OC_DB_NAME}" >"${BACKUP_DESTINATION}/${time_stamp}_owncloud.sql" 2>stderr.txt)
+  stderr=$(cat stderr.txt)
 
+  log "${stdout}"
+  log "${stderr}"
+
+  if [ "${#stderr}" -gt 0 ]; then
+    log "Error: SQL backup failed."
+    errors_found=$((errors_found + 1))
+  fi
+
+  echo "" >stderr.txt
+}
+
+backup_config_file() {
   log "-> Backup owncloud configuration file..."
-  cp "${configPath}" "${configPath}.back"
+
+  stdout=$(cp "${configPath}" "${configPath}.back" 2>stderr.txt)
+  stderr=$(cat stderr.txt)
+
+  log "${stdout}"
+  log "${stderr}"
+
+  if [ "${#stderr}" -gt 0 ] ; then
+    log "Error: Backup of config file failed."
+    errors_found=$((errors_found + 1))
+  fi
+
+  echo "" >stderr.txt
+}
+
+get_current_timestamp() {
+  date "+%Y-%m-%d_%H-%M-%S"
+}
+
+doBackup() {
+  configPath="${OC_INSTALLATION_PATH}/config/config.php"
+  backup_config_file
 
   log "-> Read owncloud configuration file..."
   configFile=$(<"${configPath}")
@@ -139,20 +199,22 @@ function doBackup {
   enable_maintenance_mode
 
   log "-> Create backup folder"
-  mkdir "${BACKUP_DESTINATION}" &> /dev/null
+  mkdir "${BACKUP_DESTINATION}" &>/dev/null
 
-  log "-> Create database backup..."
-  (mysqldump -u "${OC_DB_USER}" -p"${OC_DB_PASSWORD}" "${OC_DB_NAME}" > "${BACKUP_DESTINATION}/${timeNow}_owncloud.sql") &> /dev/null
-
+  create_sql_backup
   create_zip_backup
 
   disable_maintenance_mode
 
   duration=$SECONDS
-  log "-> Finished after $(($duration / 60)) minutes."
+  log "-> Finished after $(($duration / 60)) minutes with ${errors_found} errors."
 }
 
-function showHelp {
+cleanup() {
+  rm stderr.txt &>/dev/null
+}
+
+showHelp() {
   echo "oc-backup v${OCB_VERSION}"
   echo "usage: ./oc_backup [options]"
   echo ""
@@ -164,15 +226,14 @@ function showHelp {
 }
 # FUNCTIONS END
 
-if [ "${show_help}" = false ] ; then
+if [ "${show_help}" = false ]; then
   log "Start oc-backup v${OCB_VERSION}..."
 
   check_available_commands
   check_config
 
-  if [ "${cmd_zip_exists}" = true ] && [ "${cmd_sqldump_exists}" = true ] ; then
-
-    if [ "${is_config_valid}" = true ] ; then
+  if [ "${cmd_zip_exists}" = true ] && [ "${cmd_sqldump_exists}" = true ]; then
+    if [ "${is_config_valid}" = true ]; then
       doBackup
     else
       echo "OC Backup Error: Invalid config file. Missing constants: ${missing_constants}."
@@ -180,12 +241,12 @@ if [ "${show_help}" = false ] ; then
   else
     output="OC Backup Error: can not start backup, because of missing commands ("
 
-    if [ "${cmd_zip_exists}" != true ] ; then
+    if [ "${cmd_zip_exists}" != true ]; then
       output="${output}zip"
     fi
 
-    if [ "${cmd_sqldump_exists}" != true ] ; then
-      if [ "${cmd_zip_exists}" != true ] ; then
+    if [ "${cmd_sqldump_exists}" != true ]; then
+      if [ "${cmd_zip_exists}" != true ]; then
         output="${output}, "
       fi
 
@@ -194,6 +255,8 @@ if [ "${show_help}" = false ] ; then
 
     echo "${output})."
   fi
+
+  cleanup
 else
   showHelp
 fi
